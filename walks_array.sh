92805335be0f06003715b45b5dae631345954478
jsanjak <jsanjak@gmail.com>
2021-11-12 11:12:37 -0500

    adjusted walk workflow for HPC

--- walks_array.sh
+++ walks_array.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-for nwalks in 10 20
+for nwalks in {10..50..5}
 do
-    for walklen in 50 100
+    for walklen in {50..200..10}
     do
         sbatch --job-name random_walks_N${nwalks}_L${walklen} \
-        submit_walks.sh \
+	submit_walks.sh \
         $nwalks \
-        $walklen
+	$walklen
+	 
     done
 done
