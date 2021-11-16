3f53024402d7b620803aad5feb16beb501e83ea3
Sanjak <sanjakjs@ncats-2170734-m.nih.gov>
2021-11-16 12:05:25 -0500

    serialized embedding array

--- 04_embeddings_array.sh
+++ 04_embeddings_array.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-for vector_size in 16 32 64 128 256 512
+for vector_size in 32 64 128 256
 do
-    for context_window in {6..20..2}
+    for context_window in 8 10 12 14
     do
-    
-        sbatch --job-name vector_embeddings_D${vector_size}_K${context_window} \
-	submit_embeddings.sh \
-        $vector_size \
-	$context_window
-	 
+        for wfile in data/walks/*
+        do
+
+            sbatch --job-name vector_embeddings_D${vector_size}_K${context_window} \
+	        submit_embeddings.sh $wfile $vector_size $context_window
+
+        done
     done
 done
--- submit_embeddings.sh
+++ submit_embeddings.sh
@@ -3,10 +3,8 @@
 #SBATCH --mem=4g
 #SBATCH --cpus-per-task=4
 #SBATCH --ntasks=1
+#SBATHC --time=24:00:00
 #SBATCH --mail-type=END
 
-python src/model/generate_embeddings.py \
-data/walks \
-${1} \
-${2}
+python src/model/generate_embeddings.py ${1} ${2} ${3}
 
