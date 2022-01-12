b41bbc7909c16136f7b840f10d97a5107908dfd8
Sanjak <sanjakjs@ncats-2170734-m.myfiosgateway.com>
2022-01-12 13:20:37 -0500

    pharos data annotated

--- 01_get_data.sh
+++ 01_get_data.sh
@@ -3,11 +3,22 @@
 #Neo4J data from disease.ncats.io
 python src/data/query_neo4j.py
 
+#Gene target - ligand/drug data from Pharos
+python src/data/query_pharos.py
+
+#ChEMBL - CHEBI mappings
+wget -O data/raw/src1src7.txt.gz https://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/wholeSourceMapping/src_id1/src1src7.txt.gz
+gunzip data/raw/src1src7.txt.gz
+
+#ChEBI - PubChem mappings
+wget -O data/raw/src7src22.txt.gz https://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/wholeSourceMapping/src_id7/src7src22.txt.gz
+gunzip data/raw/src7src22.txt.gz
+
 #GO Annotations from NCBI
 wget -O data/raw/gene2go.gz ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene2go.gz 
 gunzip data/raw/gene2go.gz
 
-wget -O data/raw/PathwayCommons12.All.hgnc.sif.gz https://www.pathwaycommons.org/archives/PC2/v12/PathwayCommons12.All.hgnc.sif.gz > 
+wget -O data/raw/PathwayCommons12.All.hgnc.sif.gz https://www.pathwaycommons.org/archives/PC2/v12/PathwayCommons12.All.hgnc.sif.gz 
 gunzip data/raw/PathwayCommons12.All.hgnc.sif.gz
 
 #Obtain ontologies
--- src/data/construct_graph.py
+++ src/data/construct_graph.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from collections import Counter
 import pickle
 import pandas as pd
+import csv
 import obonet
 import networkx as nx
 import mygene
@@ -13,6 +14,76 @@ from goatools.anno.gaf_reader import GafReader
 from goatools.base import download_ncbi_associations
 from goatools.anno.genetogo_reader import Gene2GoReader
 
+
+def process_pharos_data(pharos_data,small_molecule_set,gene2go,gene_ontology):
+    """
+    Construct graph from pharos data
+    """
+    pharos_graph = nx.MultiGraph()
+
+    pharos_uniprot = set([i.get('UniProt') for i in pharos_data])
+    mg = mygene.MyGeneInfo()
+    mg_uniprot = mg.querymany(list(pharos_uniprot), scopes='uniprot', fields='entrezgene', species='human',returnall=True)
+    entrez_counts = Counter([i.get('query') for i in mg_uniprot['out'] if i.get('entrezgene')])
+    uniprot2entrez = {}
+    
+    for query in mg_uniprot['out']:
+        if query.get('entrezgene'):
+            if  query['query'] in uniprot2entrez:
+                uniprot2entrez[query['query']].append(query['entrezgene'])
+            else:
+                uniprot2entrez[query['query']] = [query['entrezgene']]
+
+    print("looping through pharos data\n")
+    ec = 0
+    for row in pharos_data:
+        edge = []
+        uniprot = row.get('UniProt')
+        chembl = row.get('Ligand ChEMBL ID')
+        pubchem = row.get('Ligand PubChem ID')
+        
+        #Do we have a gene id for this target?
+        
+        if uniprot in uniprot2entrez:
+            for gene in uniprot2entrez[uniprot]:
+                #print([uniprot,gene])
+                pharos_graph.add_node(int(gene),key='gene')
+                edge.append(int(gene))
+                gene_annotation = gene2go.get(int(gene))
+
+                if gene_annotation is not None:
+                    go_leafs = get_leafs(gene_ontology,[i['GO_ID'] for i in gene_annotation])
+
+                    for annot in gene_annotation:
+                        if annot['GO_ID'] in go_leafs:
+                            pharos_graph.add_node(annot['GO_ID'],label='GO')
+                            rel_types = annot.get('Qualfier')
+                            if rel_types:
+                                for rel in rel_types:
+                                    pharos_graph.add_edge(int(gene),annot['GO_ID'],key=rel)
+                            else:
+                                pharos_graph.add_edge(int(gene),annot['GO_ID'],key="associated_with")
+                
+                #Use ChEMBL first if possible, unless pathway commons ChEBI only mapped to PubChem or Pharos only gave PubChem
+                if chembl:
+                    if pubchem and (('PUBCHEM:' + pubchem) in small_molecule_set):
+                        pharos_graph.add_node(('PUBCHEM:' + pubchem),key='SmallMolecule')
+                        edge.append(('PUBCHEM:' + pubchem))
+                    else:
+                        pharos_graph.add_node(('CHEMBL:' + chembl),key='SmallMolecule')
+                        edge.append(('CHEMBL:' + chembl))
+                elif pubchem:
+                    pharos_graph.add_node(('PUBCHEM:' + pubchem),key='SmallMolecule')
+                    edge.append(('PUBCHEM:' + pubchem))
+                else:
+                    continue
+
+                if len(edge) == 2:
+                    ec += 1
+                    pharos_graph.add_edge(edge[0],edge[1],key="chemical-affects")
+    print("Total edges added {0}".format(ec))
+    print("Total unique edges in graph {0}".format(pharos_graph.number_of_edges()))
+    return pharos_graph
 
 def get_sif_genes(sif_data):
     ptc_genes = []
@@ -23,7 +94,7 @@ def get_sif_genes(sif_data):
             ptc_genes.append(i[2])
     return set(ptc_genes)
 
-def process_pathway_commons_sif(sif_data,gene2go,gene_ontology):
+def process_pathway_commons_sif(sif_data,gene2go,gene_ontology,chebi2chembl,chebi2pubchem):
     """
     Process simple interaction format data as edge list
     """
@@ -76,15 +147,23 @@ def process_pathway_commons_sif(sif_data,gene2go,gene_ontology):
                                     else:
                                         ptc_graph.add_edge(gene,annot['GO_ID'],key="associated_with")
             else:
-                ptc_graph.add_node(sif[i],label='chebi')
-                edge.append(sif[i])
+                if sif[i].strip('CHEBI:') in chebi2chembl:
+                    chembl_id = 'CHEMBL:' + chebi2chembl[sif[i].strip('CHEBI:')]
+                    ptc_graph.add_node(chembl_id,label='SmallMolecule')
+                    edge.append(chembl_id)
+                elif sif[i].strip('CHEBI:') in chebi2pubchem:
+                    pubchem_id = 'PUBCHEM:' + chebi2pubchem[sif[i].strip('CHEBI:')]
+                    ptc_graph.add_node(pubchem_id,label='SmallMolecule')
+                    edge.append(pubchem_id)
+                else:
+                    ptc_graph.add_node(sif[i],label='SmallMolecule')
+                    edge.append(sif[i])
                                 
         if len(edge)==2:
             ptc_graph.add_edge(edge[0],edge[1],key=sif[1])
-    
-    print("PTC Associated GO TERMS:\n")
-    print([n for n in ptc_graph.nodes if ptc_graph.nodes[n].get('label') ==  'GO'])
+        
     return ptc_graph
+
 
 def get_goa(goa_file):
 
@@ -166,26 +245,50 @@ def main():
 
     gene2go = get_goa(project_dir / 'data/raw/gene2go')
 
+    #CHEBI - ChEMBL mapping from UNICHEM
+    chebi2chembl={}
+    with open(project_dir / 'data/raw/src1src7.txt','r') as chembl_chebi_file:
+        chembl_chebi_file.readline()
+        for line in chembl_chebi_file.readlines():
+            data = line.strip().split('\t')
+            chebi2chembl[data[1]] = data[0]
+    
+    #CHEBI - PubChem mapping from UNICHEM
+    chebi2pubchem={}
+    with open(project_dir / 'data/raw/src7src22.txt','r') as chebi_pubchem_file:
+        chebi_pubchem_file.readline()
+        for line in chebi_pubchem_file.readlines():
+            data = line.strip().split('\t')
+            chebi2pubchem[data[0]] = data[1]
+    
+
     gard_gene_df = pd.read_csv(project_dir / 'data/raw/gard2gene.csv')
     
     gard_phen_df = pd.read_csv(project_dir / 'data/raw/gard2hpo.csv')
     gard_phen_df.drop(gard_phen_df[gard_phen_df['HPO_ID'].isin(hp_remove)].index, inplace = True)
 
-    disease_subgraph = construct_disease_subgraph(gard_gene_df, gard_phen_df,gene2go,gene_ontology,phenotype_ontology)
-    disease_subgraph.remove_nodes_from(list(nx.isolates(disease_subgraph)))
-    
-
-    disease_ontograph = nx.compose_all([disease_subgraph,phenotype_ontology.to_undirected(),gene_ontology.to_undirected()])
-    disease_ontograph.remove_nodes_from(list(nx.isolates(disease_ontograph)))
-
     #Pathway commons chemical and gene/protein interaction data https://www.pathwaycommons.org/archives/PC2/v12/
     with open(project_dir / 'data/raw/PathwayCommons12.All.hgnc.sif','r') as ptc_file:
         ptc_sif = [i.strip().split('\t') for i in ptc_file.readlines()]
 
-    ptc_graph = process_pathway_commons_sif(ptc_sif,gene2go,gene_ontology)
-    disease_ontograph = nx.compose_all([disease_ontograph,ptc_graph])
+    pharos_data = []
+    with open(project_dir / 'data/raw/pharos_all_target_ligands.csv','r') as pharos_ligand_file:
+        reader = csv.DictReader(pharos_ligand_file)
+        for row in reader:
+            pharos_data.append(row)
+    
+    ptc_graph = process_pathway_commons_sif(ptc_sif,gene2go,gene_ontology,chebi2chembl,chebi2pubchem)
+    small_molecule_set =  [n for n in ptc_graph.nodes if ptc_graph.nodes[n].get('label') ==  'SmallMolecule']
 
-    print(disease_ontograph.number_of_nodes())
+    pharos_graph = process_pharos_data(pharos_data,small_molecule_set,gene2go,gene_ontology)
+
+    disease_subgraph = construct_disease_subgraph(gard_gene_df, gard_phen_df,gene2go,gene_ontology,phenotype_ontology)
+    disease_subgraph.remove_nodes_from(list(nx.isolates(disease_subgraph)))
+
+    disease_ontograph = nx.compose_all([disease_subgraph,phenotype_ontology.to_undirected(),gene_ontology.to_undirected()])
+    disease_ontograph.remove_nodes_from(list(nx.isolates(disease_ontograph)))
+
+    disease_ontograph = nx.compose_all([disease_ontograph,ptc_graph,pharos_graph])
 
     with open(project_dir / 'data/processed/disease_ontograph.pkl', 'wb') as f:
         # Pickle the 'data' dictionary using the highest protocol available.
