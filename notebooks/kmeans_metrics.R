c1366563b3eb0623b2127f17c10b4f9aaf791bbc
Sanjak <sanjakjs@ncats-2170734-m.myfiosgateway.com>
2022-03-03 13:51:13 -0500

    walk enrichment

--- notebooks/kmeans_metrics.R
+++ notebooks/kmeans_metrics.R
@@ -4,78 +4,60 @@ library(ggplot2)
 kmean_metrics <- read.csv("data/processed/kmeans_model_metrics.csv")
 
 
+formatted_data <- data.frame(scale(kmean_metrics[,c("scattering_criteria","D_EMBED","L_WALKS","K_DIM","N_WALKS")]))
+formatted_data <- setNames(formatted_data,c("Scattering Score","Embedding Dimension","Walk Length","Context Window","Walks per Disease"))
+s_rc <- coef(lm(`Scattering Score` ~ `Embedding Dimension`*`Walk Length`*`Context Window`*`Walks per Disease`,
+           data=formatted_data))[2:11]
+
+names(s_rc) <- gsub(':',' * ',gsub('`','',names(s_rc)))
+
+src_data <- data.frame(par=names(s_rc),src=as.numeric(s_rc)) %>% arrange(src) %>% mutate(sign=ifelse(src>0,1,0))
+
+src_data$par <- factor(src_data$par,levels=src_data$par[order(src_data$src)])
+
+p <- ggplot(src_data,aes(par,src,fill=as.factor(sign))) +
+  geom_col() + coord_flip() + scale_fill_manual(values=c("red","blue"))
+p <- p + ylab("Standarized regression coefficient") + xlab("") 
+p <- p + theme_classic() + guides(fill="none")
+ggsave("fig/standardized_regression_coef.png",p,dpi=150,width=6,height=4,units = "in")
+
+#Indices over dimension
+p <- ggplot(filter(kmean_metrics,N_WALKS %in% c(20,40,60,80),K_DIM %in% c(8,12,16),L_WALKS %in% c(25,50,100,150,200)),
+            aes(D_EMBED,(scattering_criteria),color=as.factor(L_WALKS))) + geom_line() + geom_point()
+p <- p + scale_x_continuous(trans='log2',breaks=c(8,16,32,64,128,256,512),labels=c(8,16,32,64,128,256,512))
+p <- p + theme_bw() + xlab("Embedding Dimension") + ylab("Scattering Score")
+p <- p  + facet_grid(K_DIM ~ N_WALKS,labeller = label_both) + guides(color=guide_legend(title='Walk Length'))
+ggsave("fig/scattering_score.png",p,dpi=300,width=10,height=6,units = "in")
 
 
 
 
-summary(lm(scattering_criteria ~ D_EMBED*L_WALKS*K_DIM*N_WALKS,data=kmean_metrics))
-
-p <- ggplot(filter(kmean_metrics,K_DIM==10),aes(N_WALKS,scattering_criteria,color=as.factor(L_WALKS))) + geom_line() + geom_point()
-p <- p + theme_bw() + xlab("Number of walks") + ylab("Scattering Score")
-p <- p + facet_wrap(D_EMBED ~ . ,scales='free',nrow=3,ncol=4)
-p
+p <- ggplot(filter(kmean_metrics,D_EMBED>=16,N_WALKS %in% c(20,40,60,80),K_DIM %in% c(8,12,16)),
+            aes(log2(D_EMBED),(davies_bouldin),color=as.factor(L_WALKS))) + geom_line() + geom_point()
+p <- p + theme_bw() + xlab("log2(d)") + ylab("Davies-Bouldin Score")
+p  + facet_grid(K_DIM ~ N_WALKS)
 
 
-View(kmean_metrics %>% group_by(D_EMBED) %>% mutate(sh_rank=rank(-silhouette_euclidean),
-                                               db_rank=rank(davies_bouldin),
-                                               ch_rank=rank(-calinski_harabasz)) %>% 
-  mutate(rank_sum = sh_rank + db_rank +ch_rank) %>%  mutate(best_rank = rank(rank_sum)) %>% filter(best_rank < 6))
+p <- ggplot(filter(kmean_metrics,D_EMBED>=16,N_WALKS %in% c(20,40,60,80),K_DIM %in% c(8,12,16)),
+            aes(log2(D_EMBED),(calinski_harabasz),color=as.factor(L_WALKS))) + geom_line() + geom_point()
+p <- p + theme_bw() + xlab("log2(d)") + ylab("Calinksi-Harabasz Score")
+p  + facet_grid(K_DIM ~ N_WALKS)
 
 
-dim_sum <- kmean_metrics %>% group_by(D_EMBED) %>% summarize(scattering = mean(scattering_criteria))
-
-p <- ggplot(filter(kmean_metrics,D_EMBED==32), aes(N_WALKS,scattering_criteria, color=as.factor(K_DIM))) + geom_line() + geom_point()
-p <- p + theme_bw() + xlab("Number of Walks") + ylab("Cluster Score (scattering)")
-p <- p + facet_wrap(L_WALKS ~ . ,nrow=2,ncol=4)
-p
-
-
-p <- ggplot(filter(kmean_metrics,D_EMBED==512), aes(N_WALKS,davies_bouldin, color=as.factor(K_DIM))) + geom_line() + geom_point()
-p <- p + theme_bw() + xlab("Number of Walks") + ylab("Cluster Score (C-H)")
-p <- p + facet_wrap(L_WALKS ~ . ,nrow=2,ncol=4)
-p
+#Indices over number of walks
+p <- ggplot(filter(kmean_metrics,D_EMBED>=16,D_EMBED %in% c(16,32,64,128,256),K_DIM %in% c(8,12,16)),
+            aes((N_WALKS),(scattering_criteria),color=as.factor(L_WALKS))) + geom_line() + geom_point()
+p <- p + theme_bw() + xlab("log2(d)") + ylab("Scattering Score")
+p  + facet_grid(D_EMBED ~ K_DIM )
 
 
-
-p <- ggplot(filter(kmean_metrics,N_WALKS==80), aes(log2(D_EMBED),scattering_criteria, color=as.factor(L_WALKS))) + geom_line() + geom_point()
-p <- p + theme_bw() + xlab("Embedding dimension") + ylab("Cluster Score (scattering)")
-p <- p + facet_grid(K_DIM ~ .)
-p
-
-
-p <- ggplot(filter(kmean_metrics,N_WALKS==80), aes(log2(D_EMBED),scattering_criteria, color=as.factor(L_WALKS))) + geom_line() + geom_point()
-p <- p + theme_bw() + xlab("Embedding dimension") + ylab("Cluster Score (scattering)")
-p <- p + facet_grid(K_DIM ~ .)
-p
+p <- ggplot(filter(kmean_metrics,D_EMBED>=16,N_WALKS %in% c(20,40,60,80),K_DIM %in% c(8,12,16)),
+            aes(log2(D_EMBED),(davies_bouldin),color=as.factor(L_WALKS))) + geom_line() + geom_point()
+p <- p + theme_bw() + xlab("log2(d)") + ylab("Davies-Bouldin Score")
+p  + facet_grid(K_DIM ~ N_WALKS)
 
 
-p <- ggplot(filter(kmean_metrics,N_WALKS==80), aes(log2(D_EMBED),calinski_harabasz, color=as.factor(L_WALKS))) + geom_line() + geom_point()
-p <- p + theme_bw() + xlab("Embedding dimension") + ylab("Cluster Score (calinski-harabasz)")
-p <- p + facet_grid(K_DIM ~ .)
-p
-
-
-#kmean_metrics <- filter(kmean_metrics,D_EMBED==16)
-
-p <- ggplot(kmean_metrics %>% group_by(N_WALKS,D_EMBED) %>% summarise(`Cluster Score` = mean(silhouette_euclidean)),
-            aes(N_WALKS,`Cluster Score`)) + geom_line() + geom_point()
-p <- p + theme_bw() + xlab("Walks per Disease") + ylab("Cluster Score")
-p <- p + facet_wrap(D_EMBED ~ . ,scales='free',nrow=3,ncol=4)
-p
-#ggsave("fig/nwalks_sensitivity.png",p,width = 6,height=4,units='in')
-
-
-df_summ <- kmean_metrics %>% group_by(D_EMBED) %>% mutate(max_ch = max(calinski_harabasz)) %>% 
-  group_by(N_WALKS,D_EMBED,L_WALKS,K_DIM) %>%
-  summarise(scattering_criteria = mean(scattering_criteria),
-            calinski_harabasz = mean(calinski_harabasz)/max_ch) 
-
-p <- ggplot(filter(kmean_metrics,K_DIM==10),aes(N_WALKS,scattering_criteria,color=as.factor(L_WALKS))) + geom_line() + geom_point()
-p <- p + theme_bw() + xlab("Number of walks") + ylab("Scattering Score")
-p <- p + facet_wrap(D_EMBED ~ . ,scales='free',nrow=3,ncol=4)
-p
-
-p <- ggplot(filter(kmean_metrics,K_DIM==10),aes(N_WALKS,calinski_harabasz,color=as.factor(L_WALKS))) + geom_line() + geom_point()
-p <- p + theme_bw() + xlab("Number of walks") + ylab("calinski_harabasz Score")
-p <- p + facet_wrap(D_EMBED ~ . ,scales='free',nrow=3,ncol=4)
-p
+p <- ggplot(filter(kmean_metrics,D_EMBED>=16,N_WALKS %in% c(20,40,60,80),K_DIM %in% c(8,12,16)),
+            aes(log2(D_EMBED),(calinski_harabasz),color=as.factor(L_WALKS))) + geom_line() + geom_point()
+p <- p + theme_bw() + xlab("log2(d)") + ylab("Calinksi-Harabasz Score")
+p  + facet_grid(K_DIM ~ N_WALKS)
