1752de53a026088d7a7dfa1ca47c7319646808d3
Sanjak <sanjakjs@ncats-2170734-m.myfiosgateway.com>
2022-04-11 15:52:22 -0400

    updated get_data.sh to pull from palantir

--- 01_get_data.sh
+++ 01_get_data.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-#Neo4J data from disease.ncats.io
-python src/data/query_neo4j.py
+export $(cat config.env | xargs)
+
+#GARD2.0 Data Lake data from Palantir
+curl -X GET -o "data/raw/gard2gene_palantir.csv" -H "Authorization: Bearer ${PALANTIR_KEY}" \
+"https://nidap.nih.gov/foundry-data-proxy/api/dataproxy/datasets/\
+${GENE_DATA_RID}/\
+branches/master/csv?includeColumnNames=true"
+
+curl -X GET -o "data/raw/gard2hpo_palantir.csv" -H "Authorization: Bearer ${PALANTIR_KEY}" \
+"https://nidap.nih.gov/foundry-data-proxy/api/dataproxy/datasets/\
+${PHENOTYPE_DATA_RID}/\
+branches/master/csv?includeColumnNames=true"
 
 #Gene target - ligand/drug data from Pharos
 python src/data/query_pharos.py
