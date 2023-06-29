70787a56f150941037c14c1be9c64b2b0cb7b8bc
Jess Binder <40614665+JessBinder@users.noreply.github.com>
2023-06-29 07:35:06 -0600

    Update submit_randomize_graph.sh

--- src/scripts/submit_randomize_graph.sh
+++ src/scripts/submit_randomize_graph.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-#SBATCH --mem=8g
+#SBATCH --mem=16g
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
-#SBATCH --time=24:00:00
+#SBATCH --time=10-00:00:00
 #SBATCH --mail-type=END
 
 python src/data/randomize_graph.py data/processed/disease_ontograph.pkl ${1}
